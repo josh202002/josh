43bc8c60589a60ccd41530d8f877921d8d95f4cb
Josh <joshkjayan@pm.me>
2024-02-11 14:03:17 +0530

    v7

--- assets/forms/getstarted.php
+++ assets/forms/getstarted.php
@@ -1,17 +1,15 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Get form data
-    $name = $_POST["name"];
-    $email = $_POST["email"];
-    $contact = $_POST["contact"];
-    $organization = $_POST["organization"];
-    $subscription = $_POST["subscription"];
-    $rooms = $_POST["rooms"];
-    $country = $_POST["country"];
-    
-    // Compose email message
-    $to = "info@hotler.app"; // Change this to your email address
-    $subject = "New form submission";
+    // Collect form data
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $contact = $_POST['contact'];
+    $organization = $_POST['organization'];
+    $subscription = $_POST['subscription'];
+    $rooms = $_POST['rooms'];
+    $country = $_POST['country'];
+
+    // Construct email message
     $message = "Name: $name\n";
     $message .= "Email: $email\n";
     $message .= "Contact Number: $contact\n";
@@ -19,7 +17,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message .= "Subscription: $subscription\n";
     $message .= "Number of Rooms: $rooms\n";
     $message .= "Country: $country\n";
-    
+
     // Send email
     if (mail($to, $subject, $message)) {
         echo "Thank you! Your form has been submitted.";
@@ -29,4 +27,5 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 } else {
     echo "Error: Method not allowed";
 }
+
 ?>
